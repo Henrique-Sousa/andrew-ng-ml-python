02a3fc9b410dc47f1b265df5e6763b1ac4b9bfd4
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2021-11-03 13:33:44 -0300

    add one more test to regularized logistic regression

--- src/test/logistic_regression_functions_test.py
+++ src/test/logistic_regression_functions_test.py
@@ -16,3 +16,22 @@ def test_sigmoid_with_matrix():
         [0.5, 1],
         [0, 0.5]])
     assert np.allclose(S, T)
+
+def test_cost_and_gradient_with_regularization():
+    theta_t = np.array([
+        [-2],
+        [-1],
+        [1],
+        [2]])
+    X_t = np.block([np.ones([5, 1]), np.arange(1, 16).reshape([3,5]).T/10])
+    y_t = (np.array([[1],[0],[1],[0],[1]]) >= 0.5).astype(np.int8)
+    lambda_t = 3;
+    J, grad = cost_function_with_regularization(theta_t, X_t, y_t, lambda_t);
+    assert J == pytest.approx(2.534819, 0.000001)
+    assert np.allclose(
+        grad, 
+        np.array([
+            [0.146561],
+            [-0.548558],
+            [0.724722],
+            [1.398003]]), atol=0.000001)
