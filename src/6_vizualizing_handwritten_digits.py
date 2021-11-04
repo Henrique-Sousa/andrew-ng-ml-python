ecb7cdbb539f58f6a81c74e8c5c13da4676c17f8
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2021-11-04 07:35:40 -0300

    chore: rename files

---
+++ src/6_vizualizing_handwritten_digits.py
@@ -0,0 +1,16 @@
+import numpy as np
+import scipy.io
+from display_data import display_data
+
+num_labels = 10
+
+mat = scipy.io.loadmat('./data/ex3data1.mat')
+X = mat['X']
+y = mat['y']
+
+m = X.shape[0]
+
+rand_indices = np.random.permutation(m);
+sel = X[rand_indices[0:100], :]
+
+display_data(sel);
--- src/7_multiclass_logit.py
+++ src/7_multiclass_logit.py
@@ -1,10 +1,7 @@
 import numpy as np
 import scipy.io
-from display_data import display_data
 from logistic_regression import *
-from data_preprocessing import *
 
-input_layer_size  = 400
 num_labels = 10
 
 mat = scipy.io.loadmat('./data/ex3data1.mat')
@@ -12,11 +9,6 @@ X = mat['X']
 y = mat['y']
 
 m = X.shape[0]
-
-rand_indices = np.random.permutation(m);
-sel = X[rand_indices[0:100], :]
-
-display_data(sel);
 
 lbda = 0.1
 all_theta = one_vs_all(X, y, num_labels, lbda)
